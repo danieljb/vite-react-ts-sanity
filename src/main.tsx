0c3c04ed0afcd85707b5010ae1aa3b8a3de9f8c0
Daniel J. Becker <code@danieljbecker.de>
2022-09-06 17:07:46 +0200

    Replace React 18 with 17

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
 const projectId = import.meta.env.VITE_SANITY_PROJECT_ID;
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.render(
   <>
     <React.StrictMode>
       <App config={{ title: "StrictMode", projectId }} />
     </React.StrictMode>
     <App config={{ title: "Lax", projectId }} />
-  </>
+  </>,
+  document.getElementById("root") as HTMLElement
 );
